c37fdd598360816983a9c27a94321e11524914ca
Smiercior <smiercior@gmail.com>
2023-03-13 19:58:11 +0100

    v1

---
+++ YTDownloader/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace YTDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(473, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(506, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Download selected";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(32, 66);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(152, 214);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(28, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wanted";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 293);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.button2.Location = new System.Drawing.Point(30, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(76, 329);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 31);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Del";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(328, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Found";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(328, 66);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(152, 214);
+            this.listBox2.TabIndex = 8;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Aqua;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(201, 141);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 33);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Search";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(507, 116);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 40);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Download all";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Red;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(328, 293);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(98, 35);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Clear";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(497, 188);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(114, 109);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(493, 337);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(58, 23);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "PL";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(557, 337);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(58, 23);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "ENG";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Goldenrod;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(122, 329);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(61, 32);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "File";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(627, 369);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "YTDownloader";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ YTDownloader/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+// TODO 
+/*
+Dark souls gmv warriors, minecraft parody problem
+*/ 
+
+namespace YTDownloader
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openFileDialog;
+        List<YoutubeExplode.Search.VideoSearchResult> foundVideos = new List<YoutubeExplode.Search.VideoSearchResult>();
+        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        bool blockButtons = false;
+        bool done = false;
+        EventHandler ev;
+
+        int findCommunicat = 1;
+        int downloadCommunicat = 2;
+        string downloadPath = "Download/";
+        string logPath = "Log/";
+
+        int lang = 0; // 0 - eng, 1 - pl
+        string[,] communicats = {
+            {"Searching", "Downloading", "Downloaded", "Can't download. Error!!!!", "See log file", "Downloaded all", "Can't download ", " item/items" },
+            {"Szukanie","Pobieranie", "Pobrano", "Nie można pobrać. Błąd!!!!", "Zobasz plik log", "Pobrano wszystko", "Nie można pobrać ", " pliku/plików"  }
+        };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+            button8.Enabled = false;
+            openFileDialog = new OpenFileDialog();
+
+            // Create folders if they doesn't exist
+            DirectoryInfo di;
+            di = Directory.CreateDirectory("Download"); 
+            di = Directory.CreateDirectory("Log");
+        }
+
+        private static void TimerRaised(Object myObject, EventArgs myEventArgs, Label label1, Form1 form1) // Timer for cleaning label1
+        {
+            
+            myTimer.Stop();
+            myTimer.Tick -= form1.ev;
+            label1.Text = "";
+            //form1.EndEvent();
+        }
+
+        private static void WaitTimer(Object myObject, EventArgs myEventArgs, int waitType, Label label1, bool done, Form1 form1) // Timer for blocking all buttons and show communicat until videos is found or downloaded
+        {
+            if(form1.blockButtons == false) // Block all necessari buttons
+            {
+                form1.ButtonsState(false);
+                form1.blockButtons = true;
+            }
+
+            label1.ForeColor = Color.Yellow;
+            switch (waitType)
+            {
+                case 1: // Find videos
+                    {
+                        if (!label1.Text.Contains(form1.communicats[form1.lang, 0]) || label1.Text.Length > (form1.communicats[form1.lang, 0] + "...").Length - 1) // Searching
+                        {
+                            label1.Text = form1.communicats[form1.lang,0]; 
+                        }
+                        else if (label1.Text.Contains(form1.communicats[form1.lang, 0]))
+                        {
+                            label1.Text += ".";
+                        }
+                        break;
+                    }
+                    
+                case 2: // Download videos
+                    {
+                        if (!label1.Text.Contains(form1.communicats[form1.lang, 1]) || label1.Text.Length > (form1.communicats[form1.lang, 1] + "...").Length - 1) // Downloading
+                    {
+                        label1.Text = form1.communicats[form1.lang, 1];
+                    }
+                    else if (label1.Text.Contains(form1.communicats[form1.lang, 1]))
+                    {
+                        label1.Text += ".";
+                    }
+                    break;
+                    }
+                    
+            }
+
+            if (done == true) // If task id completed
+            {
+                form1.blockButtons = false;
+                form1.ButtonsState(true);
+
+                myTimer.Stop();
+                //form1.EndEvent();
+                myTimer.Tick -= form1.ev;
+                label1.Text = "";
+            }
+
+        }
+   
+        private void ClearCommunicatAfterTime(int time) // Clear label1 after some time, time in sec
+        {
+            ev = new EventHandler((sender, e) => TimerRaised(sender, e, label1, this)); // Set timer for comunnicat
+            myTimer.Tick += ev;
+            myTimer.Interval = time * 1000;
+            myTimer.Start();
+        }
+
+        private void RaiseCommunicat(int communicatType) // Set communicats and block all buttons 
+        {
+            ev = new EventHandler((sender, e) => WaitTimer(sender, e, communicatType, label1, done, this)); // Set timer for blocking buttons and deal with communicat
+            myTimer.Tick += ev;
+            myTimer.Interval = 1000;
+            myTimer.Start();
+        }
+
+        public void ButtonsState(bool state) // Block all necessari buttons
+        {
+            Button[] buttons = { button1, button4, button5, button6 };
+            foreach(Button button in buttons)
+            {
+                button.Enabled = state;
+            }
+        }
+
+        public void EndEvent()
+        {
+            label1.Text = "";
+            myTimer.Tick -= ev;
+        }
+
+        private async Task<bool> DownloadVideo(YoutubeExplode.Search.VideoSearchResult searchResult) // Download passed video
+        {
+            try
+            {
+                var youtubeClient = new YoutubeClient();
+                var streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync(searchResult.Id);
+                var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+                await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, downloadPath + searchResult.Title + ".mp3");
+                return true;
+            }
+            catch(Exception e)
+            {
+                string error = "";
+                if (e is YoutubeExplode.Exceptions.VideoUnplayableException) error = "Can't play video";
+                else if (e is YoutubeExplode.Exceptions.VideoUnavailableException) error = "Videos is unavaiable";
+                else error = "Unnown error";
+
+                StreamWriter sw = File.AppendText(logPath + "log " + DateTime.Now.Day + " " + DateTime.Now.Month + " " + DateTime.Now.Year + ".txt");
+                sw.WriteLine("Can't download: " + searchResult.Title + "; Because: " + error);
+                sw.Close();
+                return false;
+            }
+            
+        }
+
+        private async Task<List<YoutubeExplode.Search.VideoSearchResult>> FindVideos(List<string>searchVideos) // Search for all videos from searchVideos
+        {
+            List<YoutubeExplode.Search.VideoSearchResult> foundVideos = new List<YoutubeExplode.Search.VideoSearchResult>();
+            var youtubeClient = new YoutubeClient();
+
+            foreach (string searchVideo in searchVideos) 
+            {
+                var serachResults = youtubeClient.Search.GetVideosAsync(searchVideo);
+                await foreach (var serachResult in serachResults) // If loop find video
+                {
+                    foundVideos.Add(serachResult);
+                    break;
+                }
+            }
+
+            return foundVideos;
+        }
+
+        private async void button1_Click(object sender, EventArgs e) // Download selected
+        {
+            if(listBox2.SelectedIndex >= 0)
+            {
+                var serachResult = foundVideos[listBox2.SelectedIndex];
+                done = false;
+                RaiseCommunicat(downloadCommunicat);
+                bool state = await DownloadVideo(serachResult);
+                done = true;
+                //EndEvent();
+
+                foundVideos.Remove(serachResult);
+                listBox2.Items.Remove(listBox2.SelectedItem);
+                if (state == true)
+                {
+                    label1.ForeColor = Color.Green;
+                    label1.Text = communicats[lang,2];
+                }
+                else if (state == false)
+                {
+                    label1.ForeColor = Color.Red;
+                    label1.Text = communicats[lang, 3] + Environment.NewLine + communicats[lang, 4];
+                }
+                ClearCommunicatAfterTime(2);
+            }
+            
+        }
+
+        private async void button5_Click(object sender, EventArgs e) // Download all
+        {
+            int errors = 0;
+            done = false;
+            RaiseCommunicat(downloadCommunicat);
+            foreach(var searchResult in foundVideos)
+            {
+                bool state = await DownloadVideo(searchResult);
+                listBox2.Items.RemoveAt(0);
+                if (state == false) errors += 1;
+            }
+            done = true;
+            //EndEvent();
+
+            foundVideos.Clear();
+            if (errors == 0)
+            {
+                label1.ForeColor = Color.Green;
+                label1.Text = communicats[lang, 5];
+            }
+            else
+            {
+                label1.ForeColor = Color.Red;
+                label1.Text = communicats[lang, 6] + errors.ToString() + communicats[lang, 7];
+            }
+            ClearCommunicatAfterTime(2);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Add button
+        {
+            if(textBox1.Text != "")
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Text = "";
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Del button
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private async void button4_Click(object sender, EventArgs e) // Find button
+        {
+            List<string> searchVideos = new List<string>();
+            foreach(string item in listBox1.Items) // Add all search videos to list
+            {
+                searchVideos.Add(item);
+            }
+
+            done = false;
+            RaiseCommunicat(findCommunicat);
+            foundVideos = await FindVideos(searchVideos); // Get list of all found videos
+            done = true;
+            //EndEvent();
+
+            listBox1.Items.Clear(); // Clear searchVideos list, after finding it on youtube
+            foreach(var foundVideo in foundVideos) // Add all foundVideos to listbox2
+            {
+                listBox2.Items.Add(foundVideo.Title.ToString());
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e) // Clear button
+        {
+            listBox2.Items.Clear();
+            pictureBox1.Image = null;
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e) // Someone selected video, show thubnail of it
+        {
+            try
+            {
+                pictureBox1.ImageLocation = foundVideos[listBox2.SelectedIndex].Thumbnails[0].Url;
+            }
+            catch
+            {
+                pictureBox1.ImageLocation = null;
+            }
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.button2.PerformClick();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e) // Polish language
+        {
+
+            button7.Enabled = false;
+            button8.Enabled = true;
+
+            label2.Text = "Do wyszukania";
+            label3.Text = "Znalezione";
+
+            button1.Text = "Pobierz zaznaczony";
+            button2.Text = "Dod";
+            button3.Text = "Usu";
+            button4.Text = "Szukaj";
+            button5.Text = "Pobierz wszystkie";
+            button6.Text = "Wyczyść";
+            button9.Text = "Plik";
+            lang = 1;
+        }
+
+        private void button8_Click(object sender, EventArgs e) // English language
+        {
+            button8.Enabled = false;
+            button7.Enabled = true;
+
+            label2.Text = "Wanted";
+            label3.Text = "Found";
+
+            button1.Text = "Download selected";
+            button2.Text = "Add";
+            button3.Text = "Del";
+            button4.Text = "Search";
+            button5.Text = "Download all";
+            button6.Text = "Clear";
+            button9.Text = "File";
+            lang = 0; 
+        }
+
+        private void button9_Click(object sender, EventArgs e) // Upload file button
+        {
+            openFileDialog.FileOk += UploadFile;
+            openFileDialog.ShowDialog();
+        }
+
+        private void UploadFile(object sender, EventArgs e) // Handle file upload
+        {
+            FileStream fs = File.OpenRead(openFileDialog.FileName);
+            using(StreamReader sr = new StreamReader(fs))
+            {
+                string musicTitle = "";
+                while((musicTitle = sr.ReadLine()) != null)
+                {
+                    if(musicTitle != "") listBox1.Items.Add(musicTitle);
+                }
+            }
+            fs.Close();
+        }
+    }
+    
+}
